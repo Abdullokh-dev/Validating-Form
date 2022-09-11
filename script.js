140343cadf7c95c3d242e2454046ea8982b5e8fe
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2022-09-11 21:21:30 +0500

    JS Validate Password

--- script.js
+++ script.js
@@ -6,19 +6,58 @@ const message = document.getElementById('message')
 
 // Event Listener
 let isValid = false
+let passwordMatch = false
 
 function validateForm() {
     // Using Constraint API
     isValid = form.checkValidity()
     // Style main message for an error
-    message.textContent = 'Please fill out all fields'
-    message.style.color = 'red'
-    messageContainer.style.borderColor = 'red'
+    if(!isValid) {
+        message.textContent = 'Please fill out all fields'
+        message.style.color = 'red'
+        messageContainer.style.borderColor = 'red'
+        return;
+    }
+    // Check to see if Password match
+    if(password1EL.value === password2EL.value) {
+        passwordMatch = true;
+        password1EL.style.borderColor = 'green'
+        password2EL.style.borderColor = 'green'
+    } else {
+        passwordMatch = false
+        message.textContent = 'Make sure passwords match.'
+        message.style.color = 'red'
+        messageContainer.style.borderColor = 'red'
+        password1EL.style.borderColor = 'red'
+        password2EL.style.borderColor = 'red'
+        return;
+    }
+    // If form is Valid and password match
+    if (isValid && passwordMatch) {
+        message.textContent = 'Successfully Registered!'
+        message.style.color = 'green'
+        messageContainer.style.borderColor = 'green'
+    }
+}
+
+function storeFormData() {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    }
+    // You can do something with user data
+    console.log(user)
 }
 
 function processFormData(e) {
     e.preventDefault()
     validateForm()
+    if(isValid && passwordMatch) {
+        storeFormData()
+    }
 }
 
 form.addEventListener('submit', processFormData)
